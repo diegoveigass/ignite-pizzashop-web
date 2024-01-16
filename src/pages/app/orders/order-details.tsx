7774ca36d8ce9ed8196e9c8719a644f9a7910d28
Diego Veiga <sdiegoveiga@gmail.com>
2024-01-16 19:42:59 -0300

    initial commit

---
+++ src/pages/app/orders/order-details.tsx
@@ -0,0 +1,109 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+export function OrderDetails() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Pedido: aaaaaaaaaaaaaa00eaiwwpwo30213</DialogTitle>
+        <DialogDescription>Detalhes do pedido</DialogDescription>
+      </DialogHeader>
+
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-full bg-slate-400" />
+                  <span className="font-medium text-muted-foreground">
+                    Pendente
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">Cliente</TableCell>
+              <TableCell className="flex justify-end">
+                Diego Veiga Santos
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">Telefone</TableCell>
+              <TableCell className="flex justify-end">
+                (15) 99744-6285
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">E-mail</TableCell>
+              <TableCell className="flex justify-end">
+                diego.veiga@xmobots.com.br
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="text-muted-foreground">
+                Realizado há
+              </TableCell>
+              <TableCell className="flex justify-end">há 3 minutos</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead className="text-right">Qtd.</TableHead>
+              <TableHead className="text-right">Preço</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>Pizza de calabresa</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">R$ 49,90</TableCell>
+              <TableCell className="text-right">R$ 99,80</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Pizza de mussarela</TableCell>
+              <TableCell className="text-right">1</TableCell>
+              <TableCell className="text-right">R$ 39,90</TableCell>
+              <TableCell className="text-right">R$ 39,90</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Pizza de frango</TableCell>
+              <TableCell className="text-right">1</TableCell>
+              <TableCell className="text-right">R$ 45,90</TableCell>
+              <TableCell className="text-right">R$ 45,90</TableCell>
+            </TableRow>
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total do pedido</TableCell>
+              <TableCell>R$ 198,90</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </DialogContent>
+  )
+}
